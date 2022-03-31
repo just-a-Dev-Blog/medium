1f279cb380cc49dacf69e294dc74d66c8caf4575
Daniel Djordjevic <daniel.djordjevic@outlook.fr>
2022-03-31 18:12:15 +0200

    feat(aspnetcore-exception-middleware): init

---
+++ articles/aspnetcore-exception-middleware/src/JustADevBlog.Api/Controllers/DataController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JustADevBlog.Api.Exceptions;
+
+namespace JustADevBlog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet("")]
+        public IAsyncResult Get()
+        {
+            throw new AlgoComputeException("Algo Compute Exception Message", new AlgoComputeErrorDetails("data1", 1));
+        }
+    }
+}
---
+++ articles/aspnetcore-exception-middleware/src/JustADevBlog.Api/Exceptions/IHasErrorDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace JustADevBlog.Api.Exceptions
+{
+    public interface IHasErrorDetails
+    {
+        public IErrorDetails ErrorDetails { get; }
+    }
+
+    public interface IErrorDetails { }
+}
